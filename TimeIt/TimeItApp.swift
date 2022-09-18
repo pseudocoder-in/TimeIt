53d8b503c7b289fab0bceb32b06022b841e1244d
Subhash Jha <subhashjha.mail@gmail.com>
2022-09-18 12:12:19 +0530

    Initial Commit

---
+++ TimeIt/TimeItApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeItApp.swift
+//  TimeIt
+//
+//  Created by Subhash on 18/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TimeItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
